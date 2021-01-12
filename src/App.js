8da0b018630f653ed23408a28025f67111f0458e
mriyank <mriyanksingh@gmail.com>
2021-01-13 00:05:57 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+// import Main from "./components/Main";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header"></header>
+      <div class="row">
+        <div class="column">
+          <ul class="price">
+            <li class="header">
+              Features <br />
+              <br />
+            </li>
+
+            <li>Centralized Teams</li>
+            <li>Version History</li>
+            <li>Plugin Administrator</li>
+            <li>Facebook & Instagram Ads</li>
+            <li>Design System Analytics</li>
+            <li>Unlimited cloud storage</li>
+          </ul>
+        </div>
+
+        <div class="column">
+          <ul class="price">
+            <li class="header">
+              Starter
+              <br />
+              <span class="dollar">0</span>
+            </li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </div>
+
+        <div class="column">
+          <ul class="price">
+            <li class="header">
+              Professional
+              <br />
+              <span class="dollar">25</span>
+            </li>
+
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </div>
+
+        <div class="column">
+          <ul class="price">
+            <li class="header">
+              Organization
+              <br />
+              <span class="dollar">50</span>
+            </li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
